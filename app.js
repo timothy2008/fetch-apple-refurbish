bdc33094efea7bf56ee4a6d01b14f96d69f56023
timothy2008 <homanlee21@gmail.com>
2020-04-05 16:49:11 +0800

    app.js 	find the products by regex on its name 	print the text table of matched products

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 var moment = require('moment');
+var texttable = require('text-table');
 
 const link = 'https://www.apple.com/hk/shop/refurbished/ipad';
 const networkidletimeout = 30000;
@@ -13,6 +14,8 @@ async function fetchapplerefusbishproducts(inlink, intimeout){
           timeout: intimeout
       } );
     
+      var timenow = moment.now();
+
       var products = await page.evaluate(()=>{
         return Array.from(document.querySelectorAll('#refurbished-category-grid > div > div.as-gridpage.as-gridpage-pagination-hidden > div.as-gridpage-pane > div.as-gridpage-results > ul > li')).map((ele)=>{
             let obj = {};
@@ -24,7 +27,7 @@ async function fetchapplerefusbishproducts(inlink, intimeout){
       })
     
       products.forEach((ele,ind)=>{
-        products[ind].time = moment.now();
+        products[ind].time = timenow;
         products[ind].timestr = moment(products[ind].time).format('YYYY-MM-DD HH:mm:ss');
       })
     
@@ -32,7 +35,26 @@ async function fetchapplerefusbishproducts(inlink, intimeout){
       return products;
 }
 
+function findproducts(inproductsmap, reg){
+    var matchedproducts = [];
+    inproductsmap.forEach((ele)=>{
+        var ind = ele.name.search(reg);
+        if(ind != -1)
+            matchedproducts = matchedproducts.concat(ele);
+    });
+    return matchedproducts
+}
+
 (async () => {
     var products = await fetchapplerefusbishproducts(link, networkidletimeout);
-    console.log(products)
+    var foundproduct = findproducts(products, /12\.9-inch\ iPad\ Pro/gm);
+    if(foundproduct.length > 0){
+        var foundproducttable = foundproduct.map((ele)=>{
+            return [ele.name, ele.pricestr, ele.timestr]
+        })
+        var t = texttable(foundproducttable);
+        console.log(t)
+    }else{
+        console.log("No products matched.")
+    }
 })();
